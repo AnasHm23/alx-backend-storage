ac4ae3c68184a82302da03fe8789e24f780b1c4d
Anashm24 <anashoumaid19@gmail.com>
2024-07-17 18:54:51 +0100

    orders

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,13 +8,14 @@ CREATE PROCEDURE AddBonus (
     IN score INT
 )
 BEGIN
-    DECLARE project_id;
+    DECLARE project_id INT;
 
     IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
     THEN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
-    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+
+    SELECT id INTO project_id FROM projects WHERE name = project_name LIMIT 1;
 
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, project_id, score);
