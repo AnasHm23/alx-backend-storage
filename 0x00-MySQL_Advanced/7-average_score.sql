b7c556e8782a1aef85f3b522486ad8297ea7d996
Anashm24 <anashoumaid19@gmail.com>
2024-07-18 00:46:59 +0100

    checking

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -9,8 +9,8 @@ BEGIN
     DECLARE average_score_for_user FLOAT;
 
     SELECT AVG(score) INTO average_score_for_user
-    FROM corrections
-    WHERE user_id = user_id;
+    FROM corrections AS C
+    WHERE C.user_id = user_id;
 
     UPDATE users
     SET average_score = average_score_for_user
