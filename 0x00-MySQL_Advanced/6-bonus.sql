460d216c442f910b381a5785b58eaa2eba8da487
Anashm24 <anashoumaid19@gmail.com>
2024-07-17 18:49:53 +0100

    orders

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -21,7 +21,7 @@ BEGIN
     END IF;
 
     INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
 END //
 
 DELIMITER ;
